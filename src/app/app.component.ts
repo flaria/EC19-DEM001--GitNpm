ac02122181b67da018a3e14a5b106c860c482836
Juan Fernando Rivera <jfriverap@ectotec.com>
2019-06-25 18:59:08 -0500

    Importando modulos y componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {Equipo} from './equipo';
+import {MessageService} from 'primeng/components/common/messageservice';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularTest2';
+
+  equipos: Equipo[];
+
+    constructor(private messageService: MessageService) {
+        this.equipos = [
+            {archivo: 'Florida', nombre:'Florida Flamingos', liga:'World Team Tennis'}
+        ];
+    }
+
+    selectCar(equipo: Equipo) {
+        this.messageService.add({severity: 'info', summary: 'Car Selected', 
+          detail: 'nombre:' + equipo.nombre});
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JuegosComponent } from './juegos/juegos.component';
+
+import {CarouselModule} from 'primeng/carousel';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JuegosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
