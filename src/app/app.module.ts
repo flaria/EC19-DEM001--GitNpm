b5f32c62c644009113511c237a0cf0ce8eafe0c5
ChaimCG <ChaimCG@users.noreply.github.com>
2019-06-26 08:07:29 -0500

    Agregar Carrusel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+
+import { CarouselModule } from 'primeng/carousel';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarruselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
