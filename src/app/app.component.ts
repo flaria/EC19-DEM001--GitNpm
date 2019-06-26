516ac3e6e9633b24f321e73982a35a1426b62cb3
Juan Fernando Rivera <jfriverap@ectotec.com>
2019-06-25 19:23:21 -0500

    Ajustar despliegue y equipos a desplegar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,12 +20,12 @@ export class AppComponent {
             {archivo: 'Soviets', nombre:'The Soviets', liga:'World Team Tennis'},
             {archivo: 'Cosmos', nombre:'New York Cosmos', liga:'North American Soccer League'},
             {archivo: 'Seattle', nombre:'Seattle Seawolves', liga:'Major League Rugby'},
-            {archivo: 'SanDiego', nombre:'San Diego Sockers', liga:'North American Soccer League'},
+            {archivo: 'SanDiego', nombre:'San Diego Sockers', liga:'Major Indoor Soccer League'},
             {archivo: 'Kentucky', nombre:'Kentucky Colonels', liga:'American Basketball Association'},
             {archivo: 'Cordoba', nombre:'Cafeteros de Córdoba', liga:'Liga Mexicana de Beisbol'},
             {archivo: 'Monterrey', nombre:'Sultanes de Monterrey', liga:'Liga Mexicana de Beisbol'},
             {archivo: 'NewYork', nombre:'New York Apples', liga:'World Team Tennis'},
-            {archivo: 'Chicago', nombre:'Chicago Bulls', liga:'National Basketball Association'}
+            {archivo: 'Denver', nombre:'Denver Nuggets', liga:'National Basketball Association'}
         ];
     }
 
